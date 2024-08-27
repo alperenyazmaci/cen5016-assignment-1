0940ca2345c923479253c3bb190ffb8588d83f70
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2024-08-26 23:02:15 -0400

    fix unnecessary type annotations

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import fibRoute from "./fibRoute";
 
 const app: Express = express();
-const port: number = 3000;
+const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
