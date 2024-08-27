97436071623eae7aa0fe73b4d8d69ee8eaa62e51
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2024-08-26 22:52:06 -0400

    add type for result in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -6,7 +6,7 @@ export default (req, res) => {
   const num: string = req.params.num;
 
   const fibN : number = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  let result : string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
